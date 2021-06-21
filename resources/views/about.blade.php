0ca2f8d98f04d72aad5166295ec22fd484332390
KrymmyEM <evgeny.momotov@gmail.com>
2021-06-21 10:19:29 +0300

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About page</title>
+</head>
+<body>
+	<h1>Страница с информацией</h1>
+	<p>Этот сайт был создан в виде блога, и постепенно будет расширяться и улучшаться, по мере изучени Laravel</p>
+	<a href="{{route('beginPage') }}">Начальная страница</a>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home page</title>
+</head>
+<body>
+	<h1>Домашняя страница</h1>
+	<p>Здраствуйте это моя домашня страница созданная в Laravel.</p>
+	<a href="{{ route('about') }}">Информация обо мне</a>
+</body>
+</html>
